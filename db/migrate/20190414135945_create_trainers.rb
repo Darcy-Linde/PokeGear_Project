8eed926aee6e359a127f7317acba6ac6b01fcc33
Darcy Linde <lindedarcy@gmail.com>
2019-04-15 14:14:56 -0400

    updated trainers table

--- db/migrate/20190414135945_create_trainers.rb
+++ db/migrate/20190414135945_create_trainers.rb
@@ -2,10 +2,8 @@ class CreateTrainers < ActiveRecord::Migration[5.2]
   def change
     create_table :trainers do |t|
       t.string :name
-      t.integer :money
       t.text :bio
-      t.integer :party_id
-      t.integer :inventory_id
+      t.string :profile_img
 
       t.timestamps
     end
