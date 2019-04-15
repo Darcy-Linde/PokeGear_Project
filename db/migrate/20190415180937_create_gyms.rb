24a3115d1dead0f0df7ca622567b399bb90f4851
Darcy Linde <lindedarcy@gmail.com>
2019-04-15 14:27:52 -0400

    updated gyms table

--- db/migrate/20190415180937_create_gyms.rb
+++ db/migrate/20190415180937_create_gyms.rb
@@ -1,6 +1,13 @@
 class CreateGyms < ActiveRecord::Migration[5.2]
   def change
     create_table :gyms do |t|
+      t.string :name
+      t.string :gym_img
+      t.string :badge_name
+      t.string :badge_img
+      t.string :leader_name
+      t.string :leader_img
+      t.integer :number_of_questions
 
       t.timestamps
     end
