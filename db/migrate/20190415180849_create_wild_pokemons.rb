9b06e1c5b87b306f071bbceb54106910fcf888e6
Darcy Linde <lindedarcy@gmail.com>
2019-04-15 14:34:46 -0400

    updated wild_pokemons table

--- db/migrate/20190415180849_create_wild_pokemons.rb
+++ db/migrate/20190415180849_create_wild_pokemons.rb
@@ -1,6 +1,11 @@
 class CreateWildPokemons < ActiveRecord::Migration[5.2]
   def change
     create_table :wild_pokemons do |t|
+      t.integer :pokedex_index
+      t.string :name
+      t.integer :height
+      t.integer :weight
+      t.text :description
 
       t.timestamps
     end
