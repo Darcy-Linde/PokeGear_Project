27fda1abad6cc4a296c74d642dc99601484cbdce
Darcy Linde <lindedarcy@gmail.com>
2019-04-15 14:17:44 -0400

    changed owned_pokemons table

--- db/migrate/20190414222722_create_owned_pokemons.rb
+++ db/migrate/20190414222722_create_owned_pokemons.rb
@@ -1,6 +1,6 @@
 class CreateOwnedPokemons < ActiveRecord::Migration[5.2]
   def change
-    create_table :pokemons do |t|
+    create_table :owned_pokemons do |t|
       t.integer :wild_pokemon_id
       t.integer :trainer_id
       t.string :nick_name
