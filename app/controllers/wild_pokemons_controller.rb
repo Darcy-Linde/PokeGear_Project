947df7514147b42146ec3a2dde5809b96f4b4b76
Darcy Linde <lindedarcy@gmail.com>
2019-04-16 11:54:13 -0400

    Working index page. Started Bulma styling

--- app/controllers/wild_pokemons_controller.rb
+++ app/controllers/wild_pokemons_controller.rb
@@ -1,8 +1,10 @@
 class WildPokemonsController < ApplicationController
   def index
+    @wild_pokemons = WildPokemon.all
   end
 
   def show
+    @wild_pokemon = WildPokemon.find(params[:id])
   end
-  
+
 end
