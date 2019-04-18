9d991aa5bf75db89bfc207892b53c78a98a3cba5
Darcy Linde <lindedarcy@gmail.com>
2019-04-18 11:35:17 -0400

    integrating questions into gym battle page

--- app/controllers/gyms_controller.rb
+++ app/controllers/gyms_controller.rb
@@ -2,6 +2,7 @@ class GymsController < ApplicationController
   def index
     gym = Trainer.find(current_user.id).num_badges + 1
     @gym = Gym.find_by(:index == gym)
+    @question = Trivium.easy_trivia
   end
 
   def show
