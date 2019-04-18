d01cf69ef257e3833beed2d9595446b7906c9a7e
Mouni Mullapudi <mullapudi24@gmail.com>
2019-04-18 17:24:11 -0400

    added leaderboard

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,8 @@
 class SessionsController < ApplicationController
 
   def new
+    @sorted = Trainer.order(num_badges: :desc).limit(5)
+
   end
 
   def create
@@ -17,10 +19,6 @@ class SessionsController < ApplicationController
   def destroy
     session.delete(:user_id)
     redirect_to login_path
-  end
-
-  def top_five_leaderboards
-    
   end
 
 
