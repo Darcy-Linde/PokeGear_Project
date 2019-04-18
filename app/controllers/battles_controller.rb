b7bf5565a82a2cb59091d45afa76a4927727357b
Darcy Linde <lindedarcy@gmail.com>
2019-04-18 13:26:34 -0400

    added battles view folder

--- app/controllers/battles_controller.rb
+++ app/controllers/battles_controller.rb
@@ -1,4 +1,18 @@
 class BattlesController < ApplicationController
   def show
-  end 
+  end
+
+  def new
+    @gym = Gym.find_by(index: current_user.num_badges + 1)
+    @question = Trivium.easy_trivia
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
 end
